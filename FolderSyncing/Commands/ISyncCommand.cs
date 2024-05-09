aef080e7f1a3551de2419f1acab6ff00753fc7e7
Ruben A. Lopes <91846217+LopesR1993@users.noreply.github.com>
2024-05-09 21:42:59 +0100

    Removed CancellationToken from SyncCommand and ISyncCommand

--- FolderSyncing/Commands/ISyncCommand.cs
+++ FolderSyncing/Commands/ISyncCommand.cs
@@ -8,7 +8,6 @@ namespace FolderSyncing.Commands
             [Option('i', Description = "The interval between synchronization in seconds")] string intervalSeconds,
             [Option('s', Description = "The path to the folder to be synchronized")] string sourceFolder,
             [Option('d', Description = "The path to the replica folder")] string destinationFolder,
-            [Option('l', Description = "The path to the log file.")] string logFileLocation,
-            CancellationToken cancellationToken);
+            [Option('l', Description = "The path to the log file.")] string logFileLocation);
     }
 }
